1f6932abda2cb0357179882feea8c51f3efa1dec
shredder42 <atbinder42@gmail.com>
2025-09-06 11:25:48 -0500

    8 bytes

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const inputFilePath = "messages.txt"
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
+	}
+	defer file.Close()
+
+	for {
+		b := make([]byte, 8, 8)
+		n, err := file.Read(b)
+		if err != nil {
+			if err == io.EOF {
+				os.Exit(0)
+			}
+			log.Fatal(err)
+		}
+
+		fmt.Printf("read: %s\n", string(b[:n]))
+	}
 }
