c05397315b23c9b7877a10cf5bee64d225ddf6c2
shredder42 <atbinder42@gmail.com>
2025-09-21 15:20:41 -0500

    parsing a stream

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,15 +1,15 @@
 package request
 
 import (
+	"bytes"
 	"fmt"
 	"io"
-	"log"
 	"strings"
-	"unicode"
 )
 
 type Request struct {
 	RequestLine RequestLine
+	state       requestState
 }
 
 type RequestLine struct {
@@ -18,59 +18,112 @@ type RequestLine struct {
 	Method        string
 }
 
-func RequestFromReader(reader io.Reader) (*Request, error) {
-	request, err := io.ReadAll(reader)
-	if err != nil {
-		log.Fatalf("Error reading request: %s", err)
-	}
+type requestState int
 
-	requestLine, err := parseRequestLine(request)
-	if err != nil {
-		return nil, err
-	}
+const (
+	initialized requestState = iota
+	done
+)
 
-	newRequest := Request{
-		RequestLine: *requestLine,
-	}
+const bufferSize = 8
 
-	return &newRequest, nil
+func (r *Request) parse(data []byte) (int, error) {
+	switch r.state {
+	case initialized:
+		requestLine, n, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		}
+		if n == 0 {
+			return 0, nil
+		}
+		r.RequestLine = *requestLine
+		r.state = done
+		return n, nil
+	case done:
+		return 0, fmt.Errorf("error: trying to read data in a done state")
+	default:
+		return 0, fmt.Errorf("error: unknown state")
+	}
 }
 
-func parseRequestLine(request []byte) (*RequestLine, error) {
-	stringRequest := string(request)
-	lines := strings.Split(stringRequest, "\r\n")
-	requestLine := lines[0]
-	splitLine := strings.Split(requestLine, " ")
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	buf := make([]byte, bufferSize, bufferSize)
+	readToIndex := 0
+	request := &Request{
+		state: initialized,
+	}
+
+	for request.state != done {
+		if readToIndex >= len(buf) {
+			newBuf := make([]byte, len(buf)*2)
+			copy(newBuf, buf)
+			buf = newBuf
+		}
+		n, err := reader.Read(buf[readToIndex:])
+
+		if err != nil {
+			if err == io.EOF {
+				request.state = done
+				break
+			}
+			return nil, fmt.Errorf("error reading to buffer: %s", err)
+		}
+		readToIndex += n
+
+		bytesParsed, err := request.parse(buf[:readToIndex])
+		if err != nil {
+			return nil, fmt.Errorf("error parsing request: %s", err)
+		}
+
+		newBuf := buf[bytesParsed:]
+		copy(buf, newBuf)
+		readToIndex -= bytesParsed
+	}
+
+	return request, nil
+}
+
+func parseRequestLine(data []byte) (*RequestLine, int, error) {
+	idx := bytes.Index(data, []byte("\r\n"))
+	if idx == -1 {
+		return nil, 0, nil
+	}
+	stringRequest := string(data[:idx])
+	splitLine := strings.Split(stringRequest, " ")
 
 	// validate request line
 	if len(splitLine) != 3 {
-		return nil, fmt.Errorf("malformed request line: %s", splitLine)
+		return nil, 0, fmt.Errorf("malformed request line: %s", splitLine)
 	}
 
+	method := splitLine[0]
 	for _, s := range splitLine[0] {
-		if !unicode.IsUpper(s) {
-			return nil, fmt.Errorf("method does not contain only capital letters: %s", splitLine[0])
+		if s < 'A' || s > 'Z' {
+			return nil, 0, fmt.Errorf("method does not contain only capital letters: %s", splitLine[0])
 		}
 	}
 
+	requestTarget := splitLine[1]
+
 	versionParts := strings.Split(splitLine[2], "/")
 	if len(versionParts) != 2 {
-		return nil, fmt.Errorf("malformed start-line: %s", splitLine[2])
+		return nil, 0, fmt.Errorf("malformed start-line: %s", splitLine[2])
 	}
 
 	httpPart := versionParts[0]
 	if httpPart != "HTTP" {
-		return nil, fmt.Errorf("unrecognized HTTP-version: %s", httpPart)
+		return nil, 0, fmt.Errorf("unrecognized HTTP-version: %s", httpPart)
 	}
 
 	version := versionParts[1]
 	if version != "1.1" {
-		return nil, fmt.Errorf("unrecognized HTTP-version: %s", version)
+		return nil, 0, fmt.Errorf("unrecognized HTTP-version: %s", version)
 	}
 
 	return &RequestLine{
-		Method:        splitLine[0],
-		RequestTarget: splitLine[1],
+		Method:        method,
+		RequestTarget: requestTarget,
 		HttpVersion:   version,
-	}, nil
+	}, idx + 2, nil
 }
